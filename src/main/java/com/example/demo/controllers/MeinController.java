45dea57770c3527cd6f328d4ec5ae4dd061ee9ff
Guido Müller <guido.mueller@outlook.de>
2024-04-15 16:38:09 +0200

    Initial commit

---
+++ src/main/java/com/example/demo/controllers/MeinController.java
@@ -0,0 +1,29 @@
+
+package com.example.demo.controllers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MeinController {
+
+    @GetMapping("/hallo")
+    public String hallo() {
+        return "Hallo, Welt!";
+    }
+
+    @GetMapping("/hallojson")
+    public Map<String, Object> halloJson() {
+        Map<String, Object> response = new HashMap<>();
+        response.put("nachricht", "Hallo, Welt!");
+        response.put("zeit", System.currentTimeMillis());
+        // add a json object
+        Map<String, Object> foo = new HashMap<>();
+        foo.put("bar", "baz");
+        response.put("foo", foo);
+        return response;
+    }
+}
